47ae6854f241b4a01565baa02e717ad65a97911b
Bethany Oshafi Iyamu <2854627O@student.gla.ac.uk>
2025-01-30 14:35:37 +0000

    section 5.3 done

--- rango/views.py
+++ rango/views.py
@@ -4,5 +4,3 @@ from django.http import HttpResponse
 def index(request):
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     return render(request, 'rango/index.html', context=context_dict)
-
-# Create your views here.
