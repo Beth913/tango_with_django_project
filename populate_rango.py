75bc33858db1353897e9e9b9ae6b4bbb52e40f2a
Bethany Oshafi Iyamu <2854627O@student.gla.ac.uk>
2025-01-31 23:39:17 +0000

    exercise 6.3 done

--- populate_rango.py
+++ populate_rango.py
@@ -9,25 +9,33 @@ def populate():
 
     python_pages = [
         {'title': 'Official Python Tutorial',
-         'url': 'http://docs.python.org/3/tutorial/'},
+         'url': 'http://docs.python.org/3/tutorial/',
+         'views': 97},
          {'title': 'How to Think like a Computer Scientist',
-         'url': 'http://www.greenteapress.com/thinkpython/'},
+         'url': 'http://www.greenteapress.com/thinkpython/',
+         'views': 60},
          {'title': 'Learn Python in 10 Minutes',
-         'url': 'http://www.korokithakis.net/tutorials/python/'} ]
+         'url': 'http://www.korokithakis.net/tutorials/python/',
+         'views': 13} ]
     
     django_pages = [
         {'title': 'Official Django Tutorial',
-         'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+         'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
+         'views': 156},
          {'title': 'Django Rocks',
-         'url': 'http://www.djangorocks.com/'},
+         'url': 'http://www.djangorocks.com/',
+         'views': 41},
          {'title': 'How to Tango with Django',
-         'url': 'http://www.tangowithdjango.com/'} ]
+         'url': 'http://www.tangowithdjango.com/',
+         'views': 78} ]
     
     other_pages = [
         {'title':'Bottle',
-        'url':'http://bottlepy.org/docs/dev/'},
+        'url':'http://bottlepy.org/docs/dev/',
+        'views': 27},
         {'title':'Flask',
-        'url':'http://flask.pocoo.org'} ]
+        'url':'http://flask.pocoo.org',
+        'views': 48} ]
 
     websites = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
         'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
@@ -36,21 +44,23 @@ def populate():
     for website, website_data in websites.items():
         w = add_website(website, website_data['views'], website_data['likes'])
         for p in website_data['pages']:
-            add_page(w, p['title'], p['url'])
+            add_page(w, p['title'], p['url'], p['views'])
 
     for w in Category.objects.all():
         for p in Page.objects.filter(category=w):
             print(f'- {w}: {p}')
 
-def add_page(website, title, url, views=0):
+def add_page(website, title, url, views):
     p = Page.objects.get_or_create(category=website, title=title)[0]
     p.url = url
     p.views = views
     p.save()
     return p
 
-def add_website(name, views=0, likes=0):
-    w = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
+def add_website(name, views, likes):
+    w = Category.objects.get_or_create(name=name)[0]
+    w.views = views
+    w.likes = likes
     w.save()
     return w
 
